c7825411c933cc6ad22d745c5895bdb78fa5023f
Quiqueoso <153392995+gymcode6@users.noreply.github.com.>
2024-08-12 19:01:51 -0600

    move files to sub-carpet

---
+++ Simple-Wisper-Transcriber/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-blue-400'>hola</h1>
+    </div>
+  )
+}
+
+export default App
